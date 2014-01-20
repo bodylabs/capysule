36a7c56405cda619dce8f2a7ac1a003c2aa5adec
Paul Melnikow <paul.melnikow@bodylabs.com>
2014-01-20 15:34:10 -0500

    Expose current site on capysule.Client

--- capysule/client.py
+++ capysule/client.py
@@ -4,11 +4,13 @@ from wren.client import Client as WrenClient
 class Client(WrenClient):
     def __init__(self, site=None, api_token=None, **kwargs):
         if site is None:
-            site = os.environ['CAPSULE_SITE']
+            self.site = os.environ['CAPSULE_SITE']
+        else:
+            self.site = site
         if api_token is None:
             api_token = os.environ['CAPSULE_API_TOKEN']
 
-        url = "https://%s.capsulecrm.com" % site
+        url = "https://%s.capsulecrm.com" % self.site
         super(Client, self).__init__(url, **kwargs)
 
         self.set_basic_auth(api_token, 'x')
