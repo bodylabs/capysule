640b4ae0af91c12284efc56a391e8858b898c97a
Paul Melnikow <paul.melnikow@bodylabs.com>
2014-01-21 14:28:35 -0500

    Proper handling for empty result

--- capysule/resources/parties.py
+++ capysule/resources/parties.py
@@ -89,6 +89,8 @@ class Parties(Collection):
         self.persons = Persons(*args, **kwargs)
 
     def deserialize(self, response, data, many=False):
+        if data['parties'].get('@size') == '0':
+            return [] if many else None
         if many:
             person_or_persons = data['parties']['person']
             if isinstance(person_or_persons, list):
